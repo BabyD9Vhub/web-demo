9c7282a197683398686e157a25e90cca8a0947d6
BabyD9V <idontwannauselineappforwork77@gmail.com>
2023-06-13 11:52:15 +0700

    new

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+function colorRed(){
+    document.getElementById('color').style.color = "red";
+}
+function colorBlack(){
+    document.getElementById('color').style.color = "#ffff";
+
+}
+
+function Idle(){
+    document.getElementById('idle').src="images/Idle.png";
+}
+function Slide(){
+    document.getElementById('idle').src="images/Slide.png";
+
+}
+
+// function clickC(){
+//     if(onclick === true){
+//         document.getElementById('color').style.color = "red";
+//     }else if(Event.click === true){
+//         document.getElementById('color').style.color = "black";
+//     }else{
+//         alert('Error');
+//     }
+// }
